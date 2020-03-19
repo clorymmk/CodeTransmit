d045efef9070085bca466dab228c6ddbad525776
Gaven <35368013+clorymmk@users.noreply.github.com>
2020-03-19 10:50:48 +0800

    Add files via upload

---
+++ src/mainwindow.py
@@ -0,0 +1,216 @@
+﻿'''
+@Author: your name
+@Date: 2020-03-13 09:44:52
+@LastEditTime: 2020-03-16 15:21:40
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \codeTransmit\mainwindow.py
+'''
+# This Python file uses the following encoding: utf-8
+import sys, os
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QCheckBox, QMessageBox, QTableWidgetItem
+from PySide2.QtCore import Qt, QThread
+from uiWindow import Ui_MainWindow
+
+import codecs 
+import chardet 
+import threading
+
+#转换后的编码类型
+UTF8_BOM = 0
+UTF8 = 1
+GB2312 = 2
+
+FILE = 0
+FOLDER = 1
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.initForm()
+        self.connectSlots()
+
+        self.__path = None                                  #处理的文件/文件夹路径
+        self.__fileSuffix = ['.c', '.h', '.cpp', '.hpp']    #内置可勾选的文件类型
+        self.__customFileSuffix = []                        #自定义的文件类型
+        self.__encodeType = 'UTF-8-SIG'                     #默认的编码类型
+        self.__encodeTypeArr = ['UTF-8-SIG', 'utf-8', 'GB2312']
+        self.__fileOrFolder = FOLDER                        #默认处理文件夹
+        self.__mWorker = None                               #私有线程变量
+
+    def initForm(self):
+        self.ui.tableWidget.setColumnCount(2)
+        self.ui.tableWidget.setHorizontalHeaderLabels(["文件名","转换前类型"])
+        self.ui.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.ui.tableWidget.setColumnWidth(0, 165)
+        self.ui.tableWidget.setColumnWidth(1, 70)
+
+    def setFilePath(self, path):
+        self.__path = path
+
+    def getFileSuffix(self):
+        return self.__fileSuffix
+
+    def addFileSuffix(self, item):
+        self.__fileSuffix.append(item)
+
+    def removeFileSuffix(self, item):
+        self.__fileSuffix.remove(item)
+    
+    def clearFileSuffix(self):
+        self.__fileSuffix.clear()
+
+    def setEncodeType(self, type):
+        self.__encodeType = type
+
+    def connectSlots(self):
+        self.ui.btnChooseFolder.clicked.connect(self.onOpenFolderClicked)
+        self.ui.btnChooseFile.clicked.connect(self.onOpenFileClicked)
+        self.ui.btnClear.clicked.connect(self.onBtnClearClicked)
+        self.ui.comboBox.currentIndexChanged.connect(self.onCbEncodeIndexChanged)
+        self.ui.btnCustomCheck.clicked.connect(self.onCustomEncodeCheck)
+        fileTypeArr = self.ui.groupBox.findChildren(QCheckBox)
+        for index, item in enumerate(fileTypeArr):
+            item.stateChanged.connect(self.onFileTypeChanged)
+        self.ui.btnTransmit.clicked.connect(self.onTransmitClicked)
+
+    def onOpenFileClicked(self):
+        fileName = QFileDialog.getOpenFileName(self, "", ".")
+        if (fileName is None):
+            self.ui.textBrowser.append("open file failed: fileName is None!")
+            return
+
+        self.setFilePath(fileName[0])
+        self.__fileOrFolder = FILE
+        self.ui.labelPath.setText(fileName[0])
+
+    def onOpenFolderClicked(self):
+        folderName = QFileDialog.getExistingDirectory(self, "", ".")
+        if (folderName is None):
+            self.ui.textBrowser.append("open folder failed:folderName is None!")
+            return
+        
+        self.setFilePath(folderName)
+        self.__fileOrFolder = FOLDER
+        self.ui.labelPath.setText(folderName)
+
+    def onBtnClearClicked(self):
+        self.ui.textBrowser.clear()
+        self.ui.tableWidget.clearContents()
+        self.ui.tableWidget.setRowCount(0)
+
+    def onCbEncodeIndexChanged(self, index):
+        self.setEncodeType(self.__encodeTypeArr[index])
+        self.ui.textBrowser.append("Set encodeType: %s" % self.__encodeTypeArr[index])
+    
+    def onCustomEncodeCheck(self):
+        customStr = self.ui.leditCustomEncode.text()
+        customArr = customStr.split(' ')
+
+        for index, item in enumerate(customArr):
+            if(len(item) < 2):
+                QMessageBox.critical(self, "Error!", "自定义后缀无效:长度至少为2!")
+                return
+            if(item[0] != '.'):
+                QMessageBox.critical(self, "Error!", "自定义后缀无效:必须以 '.' 打头!")
+                return
+            if(item.count('.',) > 1):
+                QMessageBox.critical(self, "Error!", "自定义后缀无效:一种格式中不能出现多个 '.'!")
+                return
+            #移除后缀重复的元素
+            if (item not in self.__fileSuffix) and (item not in self.__customFileSuffix):
+                self.__customFileSuffix.append(item)
+    
+    def onFileTypeChanged(self, state):
+        checkBox = QCheckBox.sender(self)
+        itemText = checkBox.text()
+        if(False == state):
+            if itemText in self.__fileSuffix:
+                self.removeFileSuffix(itemText)
+        else:
+            if itemText not in self.__fileSuffix:
+                self.addFileSuffix(itemText)
+            if itemText in self.__customFileSuffix:
+                self.__customFileSuffix.remove(itemText)
+
+    def enableWidgets(self, enabled):
+        self.ui.groupBoxPath.setEnabled(enabled)
+        self.ui.groupBoxEncode.setEnabled(enabled)
+        self.ui.btnTransmit.setEnabled(enabled)
+        self.ui.btnClear.setEnabled(enabled)
+
+    def onTransmitClicked(self):
+        if self.__path is None:
+            QMessageBox.warning(self, "Warning!", "请先选择'文件'或'文件夹'路径!")
+            return
+
+        self.ui.tableWidget.clearContents()
+        self.ui.tableWidget.setRowCount(0)
+
+        if self.__fileOrFolder == FILE:
+            self.convert(self.__path, self.__encodeType)
+        elif self.__fileOrFolder == FOLDER:
+            if 0 == len(self.__fileSuffix) and 0 == len(self.__customFileSuffix):
+                QMessageBox.critical(self, "Error!", "请设置需要处理的文件后缀格式!")
+                return
+            self.enableWidgets(False)
+            self.__mWorker = threading.Thread(target=self.explore, args=(self.__path,))
+            self.__mWorker.start()
+        else:
+            QMessageBox.critical(self, "Error!", "文件类型错误，无法转换!")
+        QApplication.processEvents()
+
+    def explore(self, dir):
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                suffix = os.path.splitext(file)[1]
+                # if suffix == '.h' or suffix == '.c' or suffix == '.cpp' or suffix == '.hpp' or suffix == '.bat': 
+                #     path = os.path.join(root,file)
+                #     self.convert(path)
+                if self.__fileSuffix:
+                    for item in self.__fileSuffix:
+                        if(item == suffix):
+                            path = os.path.join(root,file)
+                            self.convert(path, self.__encodeType)
+                if self.__customFileSuffix:
+                    for item in self.__customFileSuffix:
+                        if(item == suffix):
+                            path = os.path.join(root,file)
+                            self.convert(path, self.__encodeType)
+
+        self.ui.textBrowser.append("explore over!")
+        self.enableWidgets(True)
+
+    def convert(self, filePath, out_enc="UTF-8-SIG"):
+        try: 
+            content = codecs.open(filePath,'rb').read()
+            source_encoding = chardet.detect(content)['encoding']
+
+            if source_encoding != None:
+                if source_encoding == out_enc:
+                    # self.ui.textBrowser.append("此文件格式无需转换: %s" % filePath)
+                    return
+                
+                rouCount = self.ui.tableWidget.rowCount()
+                self.ui.tableWidget.insertRow(rouCount)
+                self.ui.tableWidget.setItem(rouCount, 0, QTableWidgetItem(filePath.split('\\')[-1]))
+                self.ui.tableWidget.setItem(rouCount, 1, QTableWidgetItem(source_encoding))
+                self.ui.tableWidget.item(rouCount, 0).setTextAlignment(Qt.AlignCenter)
+                self.ui.tableWidget.item(rouCount, 1).setTextAlignment(Qt.AlignCenter)
+
+                content = content.decode(source_encoding).encode(out_enc)
+                codecs.open(filePath,'wb').write(content)
+            else :
+                self.ui.textBrowser.append("此文件无法识别编码: %s" % filePath)
+        except Exception as err: 
+            self.ui.textBrowser.append("%s:%s"%(filePath, err))
+    
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ src/uiWindow.py
@@ -0,0 +1,308 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(623, 487)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_4.addWidget(self.label_2)
+
+        self.labelPath = QLabel(self.centralwidget)
+        self.labelPath.setObjectName(u"labelPath")
+
+        self.horizontalLayout_4.addWidget(self.labelPath)
+
+        self.horizontalLayout_4.setStretch(1, 1)
+
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout_9 = QVBoxLayout()
+        self.verticalLayout_9.setObjectName(u"verticalLayout_9")
+        self.groupBoxPath = QGroupBox(self.centralwidget)
+        self.groupBoxPath.setObjectName(u"groupBoxPath")
+        self.verticalLayout_5 = QVBoxLayout(self.groupBoxPath)
+        self.verticalLayout_5.setSpacing(2)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_5.setContentsMargins(-1, 5, 2, -1)
+        self.tabWidget = QTabWidget(self.groupBoxPath)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.verticalLayout_7 = QVBoxLayout(self.tab)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.verticalLayout_7.setContentsMargins(2, 2, 2, 2)
+        self.frame_2 = QFrame(self.tab)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.NoFrame)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.btnChooseFile = QPushButton(self.frame_2)
+        self.btnChooseFile.setObjectName(u"btnChooseFile")
+
+        self.horizontalLayout_2.addWidget(self.btnChooseFile)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout_7.addWidget(self.frame_2)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_7.addItem(self.verticalSpacer)
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.verticalLayout_8 = QVBoxLayout(self.tab_2)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.verticalLayout_8.setContentsMargins(2, 2, 2, 2)
+        self.frame_3 = QFrame(self.tab_2)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.NoFrame)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_4 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.btnChooseFolder = QPushButton(self.frame_3)
+        self.btnChooseFolder.setObjectName(u"btnChooseFolder")
+
+        self.horizontalLayout_3.addWidget(self.btnChooseFolder)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_3)
+
+        self.groupBox = QGroupBox(self.frame_3)
+        self.groupBox.setObjectName(u"groupBox")
+        self.gridLayout_3 = QGridLayout(self.groupBox)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.cbTxtFile = QCheckBox(self.groupBox)
+        self.cbTxtFile.setObjectName(u"cbTxtFile")
+
+        self.gridLayout_3.addWidget(self.cbTxtFile, 1, 1, 1, 1)
+
+        self.cbJavaFile = QCheckBox(self.groupBox)
+        self.cbJavaFile.setObjectName(u"cbJavaFile")
+
+        self.gridLayout_3.addWidget(self.cbJavaFile, 1, 3, 1, 1)
+
+        self.cbCppFile = QCheckBox(self.groupBox)
+        self.cbCppFile.setObjectName(u"cbCppFile")
+        self.cbCppFile.setChecked(True)
+
+        self.gridLayout_3.addWidget(self.cbCppFile, 0, 2, 1, 1)
+
+        self.label_3 = QLabel(self.groupBox)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setWordWrap(True)
+
+        self.gridLayout_3.addWidget(self.label_3, 8, 0, 1, 4)
+
+        self.cbCFile = QCheckBox(self.groupBox)
+        self.cbCFile.setObjectName(u"cbCFile")
+        self.cbCFile.setChecked(True)
+
+        self.gridLayout_3.addWidget(self.cbCFile, 0, 0, 1, 1)
+
+        self.cbBatFile = QCheckBox(self.groupBox)
+        self.cbBatFile.setObjectName(u"cbBatFile")
+
+        self.gridLayout_3.addWidget(self.cbBatFile, 1, 0, 1, 1)
+
+        self.cbPyFile = QCheckBox(self.groupBox)
+        self.cbPyFile.setObjectName(u"cbPyFile")
+
+        self.gridLayout_3.addWidget(self.cbPyFile, 1, 2, 1, 1)
+
+        self.cbMFile = QCheckBox(self.groupBox)
+        self.cbMFile.setObjectName(u"cbMFile")
+
+        self.gridLayout_3.addWidget(self.cbMFile, 2, 0, 1, 1)
+
+        self.cbHFile = QCheckBox(self.groupBox)
+        self.cbHFile.setObjectName(u"cbHFile")
+        self.cbHFile.setChecked(True)
+
+        self.gridLayout_3.addWidget(self.cbHFile, 0, 1, 1, 1)
+
+        self.cbHppFile = QCheckBox(self.groupBox)
+        self.cbHppFile.setObjectName(u"cbHppFile")
+        self.cbHppFile.setChecked(True)
+
+        self.gridLayout_3.addWidget(self.cbHppFile, 0, 3, 1, 1)
+
+        self.leditCustomEncode = QLineEdit(self.groupBox)
+        self.leditCustomEncode.setObjectName(u"leditCustomEncode")
+
+        self.gridLayout_3.addWidget(self.leditCustomEncode, 6, 0, 1, 2)
+
+        self.btnCustomCheck = QPushButton(self.groupBox)
+        self.btnCustomCheck.setObjectName(u"btnCustomCheck")
+
+        self.gridLayout_3.addWidget(self.btnCustomCheck, 6, 2, 1, 2)
+
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_3.addWidget(self.label, 5, 0, 1, 4)
+
+
+        self.verticalLayout_4.addWidget(self.groupBox)
+
+
+        self.verticalLayout_8.addWidget(self.frame_3)
+
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.verticalLayout_5.addWidget(self.tabWidget)
+
+
+        self.verticalLayout_9.addWidget(self.groupBoxPath)
+
+        self.groupBoxEncode = QGroupBox(self.centralwidget)
+        self.groupBoxEncode.setObjectName(u"groupBoxEncode")
+        self.verticalLayout_6 = QVBoxLayout(self.groupBoxEncode)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_4 = QLabel(self.groupBoxEncode)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setStyleSheet(u"")
+        self.label_4.setFrameShape(QFrame.NoFrame)
+
+        self.horizontalLayout_5.addWidget(self.label_4)
+
+        self.comboBox = QComboBox(self.groupBoxEncode)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+
+        self.horizontalLayout_5.addWidget(self.comboBox)
+
+        self.horizontalLayout_5.setStretch(1, 1)
+
+        self.verticalLayout_6.addLayout(self.horizontalLayout_5)
+
+
+        self.verticalLayout_9.addWidget(self.groupBoxEncode)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_9)
+
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.horizontalLayout_6 = QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.btnTransmit = QPushButton(self.centralwidget)
+        self.btnTransmit.setObjectName(u"btnTransmit")
+
+        self.horizontalLayout_6.addWidget(self.btnTransmit)
+
+        self.btnClear = QPushButton(self.centralwidget)
+        self.btnClear.setObjectName(u"btnClear")
+
+        self.horizontalLayout_6.addWidget(self.btnClear)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_6.addItem(self.horizontalSpacer_3)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_6)
+
+        self.tableWidget = QTableWidget(self.centralwidget)
+        self.tableWidget.setObjectName(u"tableWidget")
+
+        self.verticalLayout_2.addWidget(self.tableWidget)
+
+        self.verticalLayout_2.setStretch(1, 1)
+
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        self.horizontalLayout.setStretch(1, 1)
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.textBrowser = QTextBrowser(self.centralwidget)
+        self.textBrowser.setObjectName(u"textBrowser")
+
+        self.verticalLayout.addWidget(self.textBrowser)
+
+        self.verticalLayout.setStretch(1, 5)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(1)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u7f16\u7801\u8f6c\u6362\u5de5\u5177V1.0.0(2020.03.16)", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u5f53\u524d\u8def\u5f84:", None))
+        self.labelPath.setText("")
+        self.groupBoxPath.setTitle(QCoreApplication.translate("MainWindow", u"\u8def\u5f84\u8bbe\u7f6e", None))
+        self.btnChooseFile.setText(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u6587\u4ef6...", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u6587\u4ef6", None))
+        self.btnChooseFolder.setText(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u6587\u4ef6\u5939...", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"\u9012\u5f52\u5904\u7406\u6587\u4ef6\u5939\u4e2d\u4ee5\u4e0b\u7c7b\u578b", None))
+        self.cbTxtFile.setText(QCoreApplication.translate("MainWindow", u".txt", None))
+        self.cbJavaFile.setText(QCoreApplication.translate("MainWindow", u".java", None))
+        self.cbCppFile.setText(QCoreApplication.translate("MainWindow", u".cpp", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u5982\u679c\u81ea\u5b9a\u4e49\u591a\u79cd\u6587\u4ef6\u7c7b\u578b\uff0c\u4e0d\u540c\u7c7b\u578b\u4e4b\u95f4\u7528\u7a7a\u683c\u5206\u5f00\uff0c\u5982\uff1a.html .xml", None))
+        self.cbCFile.setText(QCoreApplication.translate("MainWindow", u".c", None))
+        self.cbBatFile.setText(QCoreApplication.translate("MainWindow", u".bat", None))
+        self.cbPyFile.setText(QCoreApplication.translate("MainWindow", u".py", None))
+        self.cbMFile.setText(QCoreApplication.translate("MainWindow", u".m", None))
+        self.cbHFile.setText(QCoreApplication.translate("MainWindow", u".h", None))
+        self.cbHppFile.setText(QCoreApplication.translate("MainWindow", u".hpp", None))
+        self.btnCustomCheck.setText(QCoreApplication.translate("MainWindow", u"\u5e94\u7528", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u81ea\u5b9a\u4e49\u7c7b\u578b", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u6587\u4ef6\u5939", None))
+        self.groupBoxEncode.setTitle(QCoreApplication.translate("MainWindow", u"\u7f16\u7801\u8bbe\u7f6e", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"\u8f6c\u6362\u540e\u7684\u7f16\u7801:", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"UTF-8 BOM", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"UTF-8", None))
+        self.comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"GB2312", None))
+
+        self.btnTransmit.setText(QCoreApplication.translate("MainWindow", u"\u8f6c\u6362", None))
+        self.btnClear.setText(QCoreApplication.translate("MainWindow", u"\u6e05\u5c4f", None))
+    # retranslateUi
+
